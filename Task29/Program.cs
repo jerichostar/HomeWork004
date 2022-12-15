84e53163f64cf73b580f5ff9b38668a449d551fd
jerichostar <103570234+jerichostar@users.noreply.github.com>
2022-12-15 20:14:51 +0300

    Task29 NOT Done

---
+++ Task29/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+
+*/
+
+int[] GetMassiv ()
+{
+    int[] massiv = new int[8];
+    Random rnd = new Random(); 
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        massiv[i] = rnd.Next(0,2);
+
+    }
+
+    return massiv;
+}
+int[] array = GetMassiv();
+
+for (int i = 0; i < array.Length; i++)
+    {
+    
+        Console.Write($"{array[i]} ");
+    }
